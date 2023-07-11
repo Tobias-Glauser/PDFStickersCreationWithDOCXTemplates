08020ffb1bfd3ff9656cbf283bb1d8cc18a24382
Tobias Glauser <tobiasglauser2006@gmail.com>
2023-07-11 22:14:29 +0200

    Added tmp docx instead of permanent docx

--- pdf_generation/pdf_generation.py
+++ pdf_generation/pdf_generation.py
@@ -6,10 +6,10 @@ class PDFGenerator:
     def __init__(self):
         self.libreoffice_path = "C:\\Users\\glautob\\Downloads\\LibreOfficePortable\\LibreOfficeWriterPortable.exe"
 
-    def generate_pdf(self, docx_file_path):
+    def generate_pdf(self, save_directory_path, docx_file_path):
 
         command = "\"" + self.libreoffice_path + "\" --headless --convert-to pdf --outdir \"" + \
-                  os.path.dirname(docx_file_path) + "\" \"" + docx_file_path + "\""
+                  save_directory_path + "\" \"" + docx_file_path + "\""
 
         ret_code = subprocess.call(command)
 
--- sticker/sticker_generation.py
+++ sticker/sticker_generation.py
@@ -19,8 +19,8 @@ class StickerGenerator:
         time.sleep(1.5)
         main_path = os.path.join(os.getcwd(), "templates")
         template_path = os.path.join(main_path, sticker.template)
-        print(main_path)
         template = DocxTemplate(template_path)
+        docx_save_path = os.path.join(os.getcwd(), "tmp", os.path.basename(save_file_path).replace(".pdf", ".docx"))
 
         to_fill_in = {}
         for data in sticker.data:
@@ -31,18 +31,18 @@ class StickerGenerator:
                 to_fill_in[data.template_name] = data.selected_value
 
         template.render(to_fill_in)
-        template.save(save_file_path.replace(".pdf", ".docx"))
+        template.save(docx_save_path)
 
-        StickerGenerator.thread = Thread(target=generate_pdf, args=[save_file_path])
+        StickerGenerator.thread = Thread(target=generate_pdf, args=[save_file_path, docx_save_path])
         StickerGenerator.thread.start()
 
 
-def generate_pdf(save_file_path):
+def generate_pdf(save_file_path, docx_save_path):
     file_existed = os.path.exists(save_file_path)
     now = datetime.now()
 
     pdf_generator = PDFGenerator()
-    pdf_generator.generate_pdf(save_file_path.replace(".pdf", ".docx"))
+    pdf_generator.generate_pdf(os.path.dirname(save_file_path), docx_save_path)
     timeout = 0
     while not ((os.path.exists(save_file_path) or file_existed) and now.timestamp() < os.path.getmtime(save_file_path)):
         time.sleep(0.1)
@@ -50,8 +50,7 @@ def generate_pdf(save_file_path):
         timeout += 1
         if timeout > 100:
             raise Exception("Timeout while waiting for pdf generation")
-        # if os.path.exists(save_file_path):
-        #     print(os.path.exists(save_file_path), now.timestamp(), os.path.getctime(save_file_path))
 
+    os.remove(docx_save_path)
     os.startfile(save_file_path)
     print("Sticker generated.")
